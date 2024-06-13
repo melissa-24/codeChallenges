a99ed2b2b68216f9da578f0a5a4d5b8f3e01fc2c
melissa-24 <mlongenberger@codingdojo.com>
2024-06-12 21:01:14 -0400

    new challenge

---
+++ Python/CodeWars/SortMyTextbooks.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/5a07e5b7ffe75fd049000051/train/python
+# HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
+
+# The sorting should NOT be case sensitive
+
+def sorter(textbooks):
+    new = sorted(textbooks, key=str.lower)
+    print(new)
+    return new
+
+sorter(['Algebra', 'History', 'Geometry', 'English'])
+sorter(['Algebra', 'history', 'Geometry', 'english'])
